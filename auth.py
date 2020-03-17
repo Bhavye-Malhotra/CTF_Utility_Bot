0ffddd4046e9c8972d0ec25a893d227ab0b7bb00
Bhavye-Malhotra <49281065+Bhavye-Malhotra@users.noreply.github.com>
2020-03-17 21:13:21 +0530

    Add files via upload

---
+++ auth.py
@@ -0,0 +1,4 @@
+auth_token= "Njg4OTU4MDE2NDQ3NDQ3MDU3.Xm8EEg.H_CqFUzVCkOA7FWdTAaTvFqr1Ak"
+conn = "mongodb+srv://werewolf:ctfbot@ctfs-bfnmk.gcp.mongodb.net/test?retryWrites=true&w=majority"
+
+# "mongodb+srv://werewolf:ctfbot@ctfs-bfnmk.gcp.mongodb.net/test?retryWrites=true&w=majority"
