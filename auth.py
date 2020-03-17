9386d796089a8089c87eb792a8c663d86b3300a0
Bhavye-Malhotra <49281065+Bhavye-Malhotra@users.noreply.github.com>
2020-03-17 21:56:16 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,4 +1,4 @@
-auth_token= "Njg4OTU4MDE2NDQ3NDQ3MDU3.Xm8EEg.H_CqFUzVCkOA7FWdTAaTvFqr1Ak"
+auth_token= "Njg4OTU4MDE2NDQ3NDQ3MDU3.XnD6CQ.CbTZYynDTdxoX3r4YPMpWdd0ymI"
 conn = "mongodb+srv://werewolf:ctfbot@ctfs-bfnmk.gcp.mongodb.net/test?retryWrites=true&w=majority"
 
 # "mongodb+srv://werewolf:ctfbot@ctfs-bfnmk.gcp.mongodb.net/test?retryWrites=true&w=majority"
