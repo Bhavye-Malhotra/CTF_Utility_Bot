97d4d506ba4637f9903441625ac87c5f79015f95
Bhavye-Malhotra <49281065+Bhavye-Malhotra@users.noreply.github.com>
2020-03-18 09:07:04 +0530

    Delete auth.py

--- auth.py
+++
@@ -1,4 +0,0 @@
-auth_token= "Njg4OTU4MDE2NDQ3NDQ3MDU3.XnD61A.VIdGm_l-KG3N5FHfL0_jndrEebg"
-conn = "mongodb+srv://werewolf:ctfbot@ctfs-bfnmk.gcp.mongodb.net/test?retryWrites=true&w=majority"
-
-# "mongodb+srv://werewolf:ctfbot@ctfs-bfnmk.gcp.mongodb.net/test?retryWrites=true&w=majority"
